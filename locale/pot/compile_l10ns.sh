7bb282f68b41437bb0439f301f6cb9872f7fb91b
Reinis Danne <rei4dan@gmail.com>
2010-08-23 09:46:50 +0300

    locale: Set proper language code for zh_TW

--- locale/pot/compile_l10ns.sh
+++ locale/pot/compile_l10ns.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-for i in `ls -d ../??`
+shopt -s extglob
+for i in `ls -d ../?(??|??_??)`
 do
   mkdir -p ${i}/LC_MESSAGES
   msgfmt ${i}/BKChem.po -o ${i}/LC_MESSAGES/BKChem.mo
