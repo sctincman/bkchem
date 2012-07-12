f9cae58b7c1bbd803ad72b546f5091003067a9f4
Reinis Danne <rei4dan@gmail.com>
2012-07-12 12:30:30 +0300

    locale: Be more verbose when compiling MO files

--- locale/pot/compile_l10ns.sh
+++ locale/pot/compile_l10ns.sh
@@ -4,5 +4,5 @@ shopt -s extglob
 for i in `ls -d ../?(??|??_??)`
 do
   mkdir -p ${i}/LC_MESSAGES
-  msgfmt ${i}/BKChem.po -o ${i}/LC_MESSAGES/BKChem.mo
+  msgfmt -c --statistics --verbose ${i}/BKChem.po -o ${i}/LC_MESSAGES/BKChem.mo
 done
