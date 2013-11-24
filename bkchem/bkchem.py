126abf39aa19dc8b6e77b17d91a16f5de0618fc1
Reinis Danne <rei4dan@gmail.com>
2013-11-24 16:50:47 +0200

    bkchem.py: Explicitly decode program options

--- bkchem/bkchem.py
+++ bkchem/bkchem.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
   enc = sys.getfilesystemencoding()
   if not enc:
     enc = sys.getdefaultencoding()
-  opts = [unicode(i, encoding=enc) for i in sys.argv[1:]]
+  opts = [i.decode(enc) for i in sys.argv[1:]]
 
   if "-v" in opts or "--version" in opts:
     print("BKChem", config.current_BKChem_version)
