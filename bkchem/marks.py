ad951b47e6e746487cd282321bb83330120a27ab
Reinis Danne <rei4dan@gmail.com>
2013-12-04 17:47:32 +0200

    marks.py: Remove trailing spaces

--- bkchem/marks.py
+++ bkchem/marks.py
@@ -68,24 +68,29 @@ class mark( simple_parent):
   def draw( self):
     pass
 
+
   def delete( self):
     if self.paper.is_registered_object( self):
       self.unregister()
     [self.paper.delete( o) for o in self.items]
     self.items = []
 
+
   def move( self, dx, dy):
     self.x += dx
     self.y += dy
     [self.paper.move( o, dx, dy) for o in self.items]
+
 
   def move_to( self, x, y):
     dx = x - self.x
     dy = y - self.y
     self.move( dx, dy)
 
+
   def lift( self):
     [self.paper.lift( i) for i in self.items]
+
 
   def redraw( self):
     registered = self.paper.is_registered_object( self)
@@ -93,18 +98,21 @@ class mark( simple_parent):
     self.draw()
     if registered:
       self.register()
-      
+
 
   def get_svg_element( self, doc):
     pass
+
 
   def transform( self, tr):
     # do only move, the direction-using marks would need to override it and make real transform
     x, y = tr.transform_xy( self.x, self.y)
     self.move_to( x, y)
 
+
   def focus( self):
     self.set_color( "red")
+
 
   def unfocus( self):
     self.set_color( self.line_color)
@@ -113,14 +121,14 @@ class mark( simple_parent):
   def set_color( self, color):
     [self.paper.itemconfig( i, outline=color, fill=color) for i in self.items if self.paper.type( i) in ("oval",)]
     [self.paper.itemconfig( i, fill=color) for i in self.items if self.paper.type( i) in ("line","text")]
-    
+
 
   def register( self):
     [self.paper.register_id( i, self) for i in self.items]
 
+
   def unregister( self):
     [self.paper.unregister_id( i) for i in self.items]
-
 
 
   def get_package( self, doc):
@@ -205,7 +213,6 @@ class radical( mark):
                                           tags='mark')]
 
 
-
   def get_svg_element( self, doc):
     e = doc.createElement( 'ellipse')
     dom_extensions.setAttributes( e,
@@ -220,10 +227,6 @@ class radical( mark):
 
 
 
-
-
-
-
 class biradical( mark):
 
   def move( self, dx, dy):
@@ -231,6 +234,7 @@ class biradical( mark):
     self.x += dx
     self.y += dy
     self.redraw()
+
 
   def draw( self):
     if self.items:
@@ -248,7 +252,7 @@ class biradical( mark):
     self.items.append( self.paper.create_oval( x-s, y-s, x+s, y+s,
                                                fill=self.line_color, outline=self.line_color,
                                                tags='mark'))
-    
+
 
   def get_svg_element( self, doc):
     e = doc.createElement( 'g')
@@ -282,12 +286,10 @@ class biradical( mark):
 
 
 
-
 class dotted_electronpair( biradical):
   """this is the same as biradical but does not change the multiplicity of the atom"""
 
   pass
-
 
 
 
@@ -300,11 +302,13 @@ class electronpair( mark):
     mark.__init__( self, atom, x, y, size=size, auto=auto)
     self.line_width = 1
 
+
   def move( self, dx, dy):
     """marks that have a direction and not only position should be redrawn on move"""
     self.x += dx
     self.y += dy
     self.redraw()
+
 
   def draw( self):
     if self.items:
@@ -318,7 +322,7 @@ class electronpair( mark):
     x0, y0 = geometry.find_parallel( x1, y1, x2, y2, -s)[0:2]
     self.items = [self.paper.create_line( x, y, x0, y0, fill=self.line_color,
                                           width=self.line_width, tags='mark')]
-    
+
 
   def get_svg_element( self, doc):
     e = doc.createElement( 'line')
@@ -333,15 +337,13 @@ class electronpair( mark):
                                   ( 'stroke', self.line_color)))
     return e
 
+
   def transform( self, tr):
     self.x, self.y = tr.transform_xy( self.x, self.y)
     for i in self.items:
       coords = self.paper.coords( i)
       tr_coords = tr.transform_xy_flat_list( coords)
       self.paper.coords( i, tuple( tr_coords))
-
-
-
 
 
 
@@ -406,15 +408,13 @@ class plus( mark):
 
 
 
-
-
 class minus( mark):
 
   standard_size = 10
   meta__undo_simple = mark.meta__undo_simple + \
                       ('draw_circle',)
   meta__save_attrs = {"draw_circle": bool}
-  
+
 
   def __init__( self, atom, x, y, size=10, auto=1):
     mark.__init__( self, atom, x, y, size=size, auto=auto)
@@ -466,13 +466,11 @@ class minus( mark):
 
 
 
-
-
 class text_mark( mark):
 
   meta__undo_simple = mark.meta__undo_simple + ('text',)
   meta__save_attrs = {"text": str}
-    
+
 
   def __init__( self, atom, x, y, text="", size=8, auto=1):
     mark.__init__( self, atom, x, y, size=size, auto=auto)
@@ -515,8 +513,6 @@ class text_mark( mark):
 
   def _after_read_package( self):
     self.size = int( self.size)
-      
-
 
 
 
@@ -544,7 +540,6 @@ class referencing_text_mark( text_mark, mark):
 
 
 
-
 class atom_number( referencing_text_mark):
 
   refname = "number"
@@ -564,10 +559,8 @@ class oxidation_number( referencing_text_mark):
 
 
 
-
-
 class pz_orbital( mark):
-  
+
   meta__mark_positioning = 'atom'
 
 
@@ -587,13 +580,12 @@ class pz_orbital( mark):
       self.delete()
 
     ps = self._get_my_curve( num_points=20)
-    
+
     self.items = [self.paper.create_polygon( ps,
                                              outline=self.line_color,
                                              fill=self.atom.area_color,
                                              tags='mark',
                                              smooth=1)]
-
 
 
   def _get_my_curve( self, num_points=20):
@@ -614,7 +606,7 @@ class pz_orbital( mark):
     coords = self._get_my_curve( num_points=50)
     for i in range( 0, len( coords), 2):
       ps += '%.3f,%.3f ' % (coords[i],coords[i+1])
-      
+
     poly = dom_extensions.elementUnder( e, 'polygon',
                                         (( 'points', ps),
                                          ( 'stroke-width', str( 1)),
@@ -628,8 +620,6 @@ class pz_orbital( mark):
       poly.setAttribute( 'stroke', self.line_color)
 
     return e
-
-
 
 
 
@@ -647,4 +637,4 @@ def points_of_curve_eight_y( x0, y0, dx, dy, part, num_points=20):
     ps.append( y)
 
   return ps
-  
+
