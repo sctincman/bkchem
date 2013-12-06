2122ee22fc3a07b075b17bbebae01b47eca8d68c
Reinis Danne <rei4dan@gmail.com>
2013-12-06 18:24:40 +0200

    ps_cairo.py: Use relative imports for intra-package modules

--- bkchem/plugins/ps_cairo.py
+++ bkchem/plugins/ps_cairo.py
@@ -17,23 +17,31 @@
 
 #--------------------------------------------------------------------------
 
-
-from cairo_lowlevel import cairo_exporter
-from tk2cairo import tk2cairo
-import cairo
 import sys
+import cairo
+
+from .tk2cairo import tk2cairo
+from .cairo_lowlevel import cairo_exporter
+from singleton_store import Screen
+
+
+
 # try if there is PSSurface in cairo
 _x = cairo.PSSurface
 
 
-class ps_cairo_exporter( cairo_exporter):
-  """Exports PostScript via the Cairo (pycairo) library. This is the preferred PS output plugin
-as it supports unicode strings and the output is of very good quality."""
 
+class ps_cairo_exporter(cairo_exporter):
+  """Exports PostScript via the Cairo (pycairo) library.
+
+  This is the preferred PS output plugin as it supports unicode strings
+  and the output is of very good quality.
+  """
   doc_string = _("Exports Postscript via the Cairo (pycairo) library. This is the preferred PS output plugin as it supports unicode strings and the output is of very good quality.")
 
   def __init__( self, paper):
     cairo_exporter.__init__( self, paper, converter_class=tk2cairo)
+
 
   def init_surface( self):
     w, h = map( int, map( round, self.pagesize))
@@ -41,16 +49,19 @@ as it supports unicode strings and the output is of very good quality."""
     surf.set_eps( True)
     return surf
 
+
   def get_scaling( self, x, y):
     sc = self._get_scaling_ratio()
     return sc, sc
 
+
   def _get_scaling_ratio( self):
-    from singleton_store import Screen
     return 72.0/Screen.dpi
+
 
   def save( self):
     self.surface.finish()
+
 
 
 # PLUGIN INTERFACE SPECIFICATION
