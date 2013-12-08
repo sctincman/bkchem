6253ebfe85ac5adc2d218adc80ca876f8042fd3b
Reinis Danne <rei4dan@gmail.com>
2013-12-08 13:10:57 +0200

    paper.py: Use atom property to get neighbor edges

--- bkchem/paper.py
+++ bkchem/paper.py
@@ -557,8 +557,8 @@ class chem_paper( Canvas, object):
 
 
   def bonds_to_update( self, exclude_selected_bonds=True):
-    a = set().union(*(set(i) for i in (v.get_neighbor_edges() for v in self.selected
-                                                                if v.object_type == "atom")))
+    a = set().union(*(set(i) for i in (v.neighbor_edges for v in self.selected
+                                                          if v.object_type == "atom")))
     # if bond is also selected then it moves with and should not be updated
     if exclude_selected_bonds:
       return [b for b in a if b not in self.selected]
