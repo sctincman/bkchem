2c187c536a835efd62acd378fe5898043f393259
Reinis Danne <rei4dan@gmail.com>
2014-01-06 18:31:22 +0200

    doc/scripts/batch_demo1.py: Reformat doc strings

--- doc/scripts/batch_demo1.py
+++ doc/scripts/batch_demo1.py
@@ -5,9 +5,11 @@ import os
 
 
 
-def update_svgs_in_path( dir):
-  """calls update_svg for an argument file,
-  in case of directory for all .svg files in the directory"""
+def update_svgs_in_path(dir):
+  """Call update_svg for an argument file.
+
+  In case of directory for all .svg files in the directory.
+  """
 
   made = 0
   ignored = 0
@@ -31,10 +33,11 @@ def update_svgs_in_path( dir):
   print("Resaved %d files, ignored %d" % (made, ignored))
 
 
+def update_svg(f):
+  """Try to open a file in BKChem.
 
-def update_svg( f):
-  """tries to open a file in BKChem, in case of success sets font size of
-  all atoms to 12 and resaves the file."""
+  In case of success sets font size of all atoms to 12 and resaves the file.
+  """
 
   print(f, "...", end=' ')
   # App.load_CDML returns true on successful load;
@@ -55,8 +58,7 @@ def update_svg( f):
 
 
 
-# this starts the script base on the command line arguments given
-
+# Start the script base on the given command line arguments
 if Args:
   for arg in Args:
     update_svgs_in_path( arg)
