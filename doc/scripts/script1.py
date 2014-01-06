a79157f73332e3ea6bceaa8803a0d4f6f5385839
Reinis Danne <rei4dan@gmail.com>
2014-01-06 18:41:22 +0200

    doc/scripts/script1.py: Reword comments

--- doc/scripts/script1.py
+++ doc/scripts/script1.py
@@ -3,43 +3,43 @@ from __future__ import print_function
 
 import sys
 
-# at first we check the command line
+# Check the command line
 if len(sys.argv) <= 1:
   print("You have to supply a filename.")
   sys.exit()
 
-# then we import bkchem and threading
+# Import bkchem and threading
 import threading
 
 from bkchem.bkchem import bkchem
 
-# bkchem.myapp is the application instance
+# Application instance
 app = bkchem.myapp
 
-# we need to set batch mode = 1 if we want to suppress some interactive warnings,
+# Set batch mode = 1 to suppress some interactive warnings,
 # questions and mouse events
 app.in_batch_mode = 1
 
-# now we start the application in a separate thread to be able to manipulate it
-t = threading.Thread( target=app.mainloop, name='app')
-t.setDaemon( 1)
+# Start the application in a separate thread to be able to manipulate it
+t = threading.Thread(target=app.mainloop, name='app')
+t.setDaemon(1)
 t.start()
 
 
-# here comes the actual code
+# The manipulation code follows
 
-# we load the file
-app.load_CDML( sys.argv[1])
+# Load the file
+app.load_CDML(sys.argv[1])
 
-# we take all molecules from the current paper, find all the double bonds,
-# change their color to red and
+# Take all molecules from the current paper, find all the double bonds,
+# change their color to red
 for mol in app.paper.molecules:
   for bond in mol.bonds:
     if bond.order == 2:
       bond.line_color = "#aa0000"
       bond.redraw()
 
-# finally we save the result and quit
+# Save the result and quit
 app.save_CDML()
 app.destroy()
 
