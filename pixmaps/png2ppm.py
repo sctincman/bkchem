8ac714e7b8cc0cb7782791a4c368acb9d8afc313
Reinis Danne <rei4dan@gmail.com>
2014-01-06 18:49:46 +0200

    pixmaps/png2ppm.py: Don't shadow built-in file

--- pixmaps/png2ppm.py
+++ pixmaps/png2ppm.py
@@ -4,7 +4,8 @@ import PIL.Image as Image
 import os.path
 import os
 
-def w_to_grey( i):
+
+def w_to_grey(i):
   if i == 0:
     return 200
   elif i == 255:
@@ -12,17 +13,19 @@ def w_to_grey( i):
   else:
     return i
 
-for file in os.listdir( os.getcwd()):
-  if os.path.isfile( file):
-    pth = os.path.splitext( file)
+
+for f in os.listdir(os.getcwd()):
+  if os.path.isfile(f):
+    pth = os.path.splitext(f)
     if pth[1] == '.png':
-      img = Image.open( file)
+      img = Image.open(f)
       size = img.size
       bands = img.split()
-      if len( bands) == 4:
+      if len(bands) == 4:
         r, g, b, a = bands
-        img = Image.new( "RGB", size, (200, 200, 200))
-        img.paste( Image.merge( "RGB", (r,g,b)), (0, 0, size[0], size[1]), a)
+        img = Image.new("RGB", size, (200, 200, 200))
+        img.paste(Image.merge("RGB", (r,g,b)), (0, 0, size[0], size[1]), a)
 
-      img.convert('RGB').save( pth[0]+'.ppm','PPM')
+      img.convert('RGB').save(pth[0]+'.ppm', 'PPM')
       print("Converting %s.png to %s.ppm" % (pth[0], pth[0]))
+
