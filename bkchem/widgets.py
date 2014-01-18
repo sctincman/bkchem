8f7bfce05b3f6d6e9244793437e18afbdd4a728e
Reinis Danne <rei4dan@gmail.com>
2014-01-18 17:10:07 +0200

    widgets.py: Remove unnecessary unicode() call

--- bkchem/widgets.py
+++ bkchem/widgets.py
@@ -489,7 +489,7 @@ class HTMLLikeInput( Tkinter.Frame, object):
     if len(event.keysym) > 1 and event.keysym in keysyms:
       if self.editPool.selection_present():
         self.editPool.delete( "anchor", "insert")
-      self.editPool.insert( 'insert', unicode( keysyms[ event.keysym]))
+      self.editPool.insert('insert', keysyms[event.keysym])
       return "break"
 
 
